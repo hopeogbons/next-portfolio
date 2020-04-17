1814e00c00cee8dda33d268bd3e585b364c3f671
Hope Ogbons <hopeogbons@gmail.com>
2020-04-17 22:26:28 +0100

    add a hire me page

---
+++ pages/hire-me.js
@@ -0,0 +1,12 @@
+import Layout from '../components/Layout';
+
+const HireMe = () => (
+  <Layout title="Hire me">
+    <p>
+      You can hire me at{' '}
+      <a href="mailto:hopeogbons@gmail.com">hopeogbons@gmail.com</a>
+    </p>
+  </Layout>
+);
+
+export default HireMe;
