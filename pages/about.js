46235857b129bab5e0fa7b935c9e02a7c5199606
Hope Ogbons <hopeogbons@gmail.com>
2020-04-17 22:26:18 +0100

    add an about page

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import Layout from '../components/Layout';
+
+const About = () => (
+  <Layout title="About">
+    <p>
+      Full-stack Javascript Software Engineer with over a decade experience
+      developing high-level business intelligent software solutions.
+    </p>
+    <img src="/images/hope.jpg" alt="Hope Ogbons" height="200px" />
+  </Layout>
+);
+
+export default About;
