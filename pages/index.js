12329a93cde8861b3835901b8ed3b56c975fdfe7
Hope Ogbons <hopeogbons@gmail.com>
2020-04-17 22:26:37 +0100

    add an index page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,9 @@
-const Index = () => <div>Welcome to the homepage</div>;
+import Layout from '../components/Layout';
+
+const Index = () => (
+  <Layout title="Home">
+    <p>Welcome to the homepage</p>
+  </Layout>
+);
 
 export default Index;
