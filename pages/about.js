63425eea97637a7194ef6dbee5b4430122959583
Hope Ogbons <hopeogbons@gmail.com>
2020-04-20 23:24:59 +0100

    refactor the about us page

--- pages/about.js
+++ pages/about.js
@@ -1,13 +1,33 @@
-import Layout from '../components/Layout';
+import { Component } from 'react';
+import fetch from 'isomorphic-unfetch';
 
-const About = () => (
-  <Layout title="About">
-    <p>
-      Full-stack Javascript Software Engineer with over a decade experience
-      developing high-level business intelligent software solutions.
-    </p>
-    <img src="/images/hope.jpg" alt="Hope Ogbons" height="200px" />
-  </Layout>
-);
+import Layout from '../components/Layout';
+import Error from './_error';
+
+class About extends Component {
+  static async getInitialProps() {
+    const res = await fetch('https://api.github.com/users/hopeogbons');
+    const statusCode = res.status > 200 ? res.status : false;
+    const data = await res.json();
+    return { user: data, statusCode };
+  }
+
+  render() {
+    const { user, statusCode } = this.props;
+
+    if (statusCode) return <Error statusCode={statusCode} />;
+
+    return (
+      <Layout title="About">
+        <p>{user.bio}</p>
+        <p>
+          Full-stack Javascript Software Engineer with over a decade experience
+          developing high-level business intelligent software solutions.
+        </p>
+        <img src={user.avatar_url} alt={user.name} height="200px" />
+      </Layout>
+    );
+  }
+}
 
 export default About;
