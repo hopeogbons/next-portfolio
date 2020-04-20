add734d350ed9f2524ce2949ed3a82ae270aaad5
Hope Ogbons <hopeogbons@gmail.com>
2020-04-20 23:22:50 +0100

    create a static 404 page

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+import Layout from '../components/Layout';
+
+const Custom404 = () => {
+  return (
+    <Layout title="Error!!!">
+      <p>{`Couldn't get that page, sorry!`}</p>
+    </Layout>
+  );
+};
+
+export default Custom404;
